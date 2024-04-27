6bd53a0725b837c7b8dbd575301379e9c7c72683
Aakash Dhakal <anamoldhakal22@gmail.com>
2024-04-27 23:19:38 +0545

    Add new launch configuration for Passenger class and update showPassengerMenu method

--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,27 @@
 public class Passenger extends User {
 
+    Plane flight = new Plane();
+
+    private void showPassengerMenu() {
+        System.out.println("""
+
+
+                \t\t\t         ███████ ██   ██ ██    ██ ██████   █████  ███████ ███████
+                \t\t\t         ██      ██  ██   ██  ██  ██   ██ ██   ██ ██      ██
+                \t\t\t         ███████ █████     ████   ██████  ███████ ███████ ███████
+                \t\t\t              ██ ██  ██     ██    ██      ██   ██      ██      ██
+                \t\t\t         ███████ ██   ██    ██    ██      ██   ██ ███████ ███████
+
+
+                  \t\t       !=================== An online flight reservation system ====================!
+
+                        """);
+        flight.vline(120, '-');
+    }
+
+    public void main(String[] args) {
+        Passenger passenger = new Passenger();
+        passenger.showPassengerMenu();
+    }
+
 }
--- User.java
+++ User.java
@@ -8,7 +8,7 @@ public class User {
     public String role;
     public int numberOfSeats;
 
-    private int userId = 1000;
+    private int userId = 1;
     private ResultSet planes;
 
     private Database database = new Database();
@@ -34,15 +34,16 @@ public class User {
                 System.out.print("Enter the number of seats you want to reserve: ");
                 numberOfSeats = scanner.nextInt();
                 scanner.close();
-                // generate random reservation id
-                int reservationId = (int) (Math.random() * (999999 - 100000) + 100000);
-                database.databaseUpdate(
-                        "insert into reservations (ticket_id ,user_id, plane_id, number_of_seats) values ("
-                                + reservationId + ","
-                                + userId + ","
-                                + flight.flightId + ", "
-                                + numberOfSeats + ");");
+
                 if (flight.checkSeatCapacity(flight.flightId, numberOfSeats)) {
+                    // generate random reservation id
+                    int reservationId = (int) (Math.random() * (999999 - 100000) + 100000);
+                    database.databaseUpdate(
+                            "insert into reservations (ticket_id ,user_id, plane_id, number_of_seats) values ("
+                                    + reservationId + ","
+                                    + userId + ","
+                                    + flight.flightId + ", "
+                                    + numberOfSeats + ");");
                     System.out.println("Reservation successful. Your reservation id is " + reservationId);
                 } else {
                     System.out.println("Sorry ! The requested number of seats are not available.");
@@ -55,9 +56,35 @@ public class User {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public void showReservations(ResultSet reservations) throws Exception {
+        // show plane ticket
+
+    }
+
+    public void showTickets(ResultSet reservation) throws Exception {
+        while (reservation.next()) {
+            flight.vline(120, '-');
+            System.out.printf("%-20s %s\n", "Ticket Id:", reservation.getString("ticket_id"));
+            System.out.printf("%-20s %s\n", "Plane Id:", reservation.getString("plane_id"));
+            System.out.printf("%-20s %s\n", "Number of Seats:", reservation.getString("number_of_seats"));
+            System.out.printf("%-20s %s\n", "From:", reservation.getString("origin"));
+            System.out.printf("%-20s %s\n", "To:", reservation.getString("destination"));
+            System.out.printf("%-20s %s\n", "Departure Date:", reservation.getString("departure_date"));
+            System.out.printf("%-20s %s\n", "Departure Time:", reservation.getString("departure_time"));
+            System.out.printf("%-20s Rs %d\n", "Total Cost:",
+                    reservation.getInt("fare") * reservation.getInt("number_of_seats"));
+            flight.vline(120, '-');
+        }
+    }
+
+    public void main(String[] args) throws Exception {
         User user = new User();
-        user.reserveSeat();
+        // user.reserveSeat();
+        ResultSet reservations = user.database
+                .databaseGet(
+                        "SELECT * FROM reservations INNER JOIN planes ON reservations.plane_id = planes.id INNER JOIN users ON reservations.user_id = users.id WHERE user_id = "
+                                + userId + ";");
+        user.showTickets(reservations);
     }
 
 }
